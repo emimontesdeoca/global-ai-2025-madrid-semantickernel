91335cec9a577014c084e2141487a1093daf28d3
Emiliano Montesdeoca <emiliano.montesdeoca@intelequia.com>
2025-03-04 14:53:18 +0000

    feat: add readme

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# global-ai-2025-madrid-semantickernel
+<img align="center" src="https://globalai.community/media/joehrinh/global_header.png">
+<br>
+<br>
+
+<h1 align="center">Global AI 2025 Madrid</h1>
+
+<h3 align="center">
+<a href="https://x.com/emimontesdeocaa">Twitter</a> ·
+<a href="https://www.linkedin.com/in/emimontesdeoca/">Linkedin</a> ·
+<a href="mailto:emimontesdeoca@outlook.es">Email</a>
+</h2>
+
+This repository contains source code and presentation slides for the Global AI 2025 Madrid.
+
+## Overview
+- Showcases Semantic Kernel demos.
+- Includes code samples demonstrating AI techniques and Azure services.
+- Contains presentation slides that accompany the demos and explanations.
+
+## Project Structure
+```
+/src        -> Source code for your demos and AI solutions
+/slides     -> Presentation files
+/README.md  -> Project documentation
+```
+
+## Getting Started
+1. Clone or download this repository.
+2. Projects are in `/src`.
+3. Slides are in `/slides`.
+
+## Contributing
+1. Fork the repository.
+2. Create a feature branch for your contribution.
+3. Submit a pull request.
+
+## License
+This project is licensed under the Apache License.
--- src/SemanticKernelSession.00/Program.cs
+++ src/SemanticKernelSession.00/Program.cs
@@ -3,5 +3,92 @@ using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 using System.ComponentModel;
+using System.Net.Http.Headers;
 
 Console.WriteLine("Hello Global AI Bootcamp 2025!\n");
+
+var endpoint = Environment.GetEnvironmentVariable("OPENAI_ENDPOINT", EnvironmentVariableTarget.User);
+
+var apikey = Environment.GetEnvironmentVariable("OPENAI_APIKEY", EnvironmentVariableTarget.User);
+
+var deploymentName = "gpt-4o-mini";
+
+var builder = Kernel.CreateBuilder();
+
+var chatService = new AzureOpenAIChatCompletionService(deploymentName, endpoint!, apikey!);
+
+var kernel = builder.Build();
+
+kernel.Plugins.AddFromType<AgePlugin>();
+kernel.Plugins.AddFromType<TaskPlugin>();
+
+var promptSettings = new AzureOpenAIPromptExecutionSettings()
+{
+    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+};
+
+var history = new ChatHistory();
+history.AddSystemMessage("Hablame como si estuvieramos en 1850");
+
+while (true)
+{
+    Console.Write("Q:");
+    var prompt = Console.ReadLine();
+
+    history.AddUserMessage(prompt);
+
+    var answer = await chatService.GetChatMessageContentAsync(history, promptSettings, kernel);
+    Console.WriteLine($"A: {answer}");
+}
+
+
+internal class AgePlugin
+{
+    [KernelFunction]
+    [Description("gets the age by providing a name")]
+    public string GetAge(string name)
+    {
+        switch (name)
+        {
+            case "John":
+                return "25";
+            case "Jane":
+                return "30";
+            default:
+                break;
+        }
+
+        return "Unknown";
+    }
+}
+
+
+public class TaskPlugin
+{
+
+    [KernelFunction]
+    [Description("Add a task to the list")]
+    public void AddTask(string task)
+    {
+        TaskService.Task.Add(task);
+    }
+
+    [KernelFunction]
+    [Description("Get all tasks")]
+    public List<string> GetTasks()
+    {
+        return TaskService.Task;
+    }
+
+    [KernelFunction]
+    [Description("Remove a task from the list, make sure is the correct task")]
+    public void RemoveTask(string task)
+    {
+        TaskService.Task.Remove(task);
+    }
+}
+
+public class TaskService
+{
+    public static List<string> Task = new();
+}
